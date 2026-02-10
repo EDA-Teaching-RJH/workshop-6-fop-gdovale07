b0d70067cc9aa609350ef87084059052124c62a7
gdovale07 <ed12@kent.ac.uk>
2026-02-10 11:32:27 +0000

    part 2 code 1 and 2

--- part_2.py
+++ part_2.py
@@ -1,1 +1,15 @@
+rover_status = {
+    "battery": 100,
+    "heater": "off",
+    "camera": "standby"
+
+}
+
+print(rover_status["battery"])
+
+rover_status["battery"] = 85
+rover_status["heater"] = "on"
+rover_status["speed"] = 5
+
+print(rover_status)
 
