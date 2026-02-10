437cb63501d51b3a6cf889f9fe03e2c4d09b9f63
gdovale07 <ed12@kent.ac.uk>
2026-02-10 11:27:52 +0000

    code 2

--- part_1.py
+++ part_1.py
@@ -5,3 +5,13 @@ print(sample_bay[0])
 print(sample_bay[-1])
 
 print(len(sample_bay))
+
+for sample in sample_bay:
+    print(f"transmitting data for: {sample}")
+
+new_findings = []
+
+for _ in range(3):
+    rock = input("enter rock type:")
+    new_findings.append(rock)
+print(new_findings)
