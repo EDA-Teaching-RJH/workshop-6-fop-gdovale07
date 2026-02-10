78037a8b3da311df3fc5c9b75fe4bf41b5aa838b
gdovale07 <ed12@kent.ac.uk>
2026-02-10 11:38:00 +0000

    part 2 code 3

--- part_2.py
+++ part_2.py
@@ -13,3 +13,11 @@ rover_status["speed"] = 5
 
 print(rover_status)
 
+mission_log = [
+    {"site": "crater a", "radiation": "low", "water": False},
+    {"site": "dune b", "radiation": "high", "water": True}
+]
+
+for site in mission_log:
+    print(f"site {site['site']} has {site['radiation']} radiation.")
+    
